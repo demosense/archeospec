bd57f235e7ce584a732ca29292efee468e0aff30
Jacinto Arias <arias.jacinto@gmail.com>
2017-11-21 20:28:48 +0100

    Added residuals table

--- R/tables.R
+++ R/tables.R
@@ -31,3 +31,33 @@ table_weights <- function(signatures) {
     }
   )
 }
+
+table_residuals <- function(signatures) {
+
+  endmembers <- signatures$endmembers
+
+  lapply(
+    1:length(endmembers),
+    function(i) {
+
+      classes <- clusters[[i]]
+      ems <- endmembers[[i]]
+      data <- cbind(class = classes, signatures$data)
+      k <- length(ems)
+      emData <- data[ems,]
+      endNames <- paste0("end", 1:length(endmembers[[i]]))
+      emData <- cbind(endmember=endNames, emData)
+
+      weightsRaw <- .compute_weights(signatures, i)
+      weights <- cbind(file = row.names(signatures$data), class = clusters[[i]], weightsRaw)
+
+      residuals <- apply(cbind(signatures$data, weightsRaw), 1, FUN = function (x) {
+        original <- x[ 1:length(signatures$range) ]
+        weights  <- x[ (length(signatures$range)+1):length(x) ]
+        .get_residuals(original, weights, emData[,signatures$range])
+      })
+
+      summary(residuals)
+    }
+  )
+}
